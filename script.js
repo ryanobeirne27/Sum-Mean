0fd575bf49987c4605bbc042c3d654cefff77ee6
ryanobeirne27 <92584712+ryanobeirne27@users.noreply.github.com>
2021-12-01 17:05:11 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function play(){
+	var sum=0;
+	var avg=0;
+	var nums=[5,10,15,20];
+	
+	for(i=0;i<nums.length;i++){
+		alert(nums[i]);
+		sum+=nums[i];
+	}
+	avg=avg+sum/nums.length;
+	alert("The sum is: "+sum);
+	alert("The mean is: "+avg);
+}
+
+function play1(){
+	var sum=0;
+	var avg=0;
+	var nums=[10,20,30,40,50];
+	
+	for(i=0;i<nums.length;i++){
+		alert(nums[i]);
+		sum+=nums[i];
+	}
+	avg=avg+sum/nums.length;
+	alert("The sum is: "+sum);
+	alert("The mean is: "+avg);
+}
+
+function play2(){
+	var sum=0;
+	var avg=0;
+	var nums=[50,100,300,400];
+	
+	for(i=0;i<nums.length;i++){
+		alert(nums[i]);
+		sum+=nums[i];
+	}
+	avg=avg+sum/nums.length;
+	alert("The sum is: "+sum);
+	alert("The mean is: "+avg);
+}
+
+function play3(){
+	var sum=0;
+	var avg=0;
+	var nums=[6,26,106,406];
+	
+	for(i=0;i<nums.length;i++){
+		alert(nums[i]);
+		sum+=nums[i];
+	}
+	avg=avg+sum/nums.length;
+	alert("The sum is: "+sum);
+	alert("The mean is: "+avg);
+}
+
